cbc8126d597bddc42e06a47c5441183d55b2ae78
João Coqueiro <jaaum.p95@gmail.com>
2022-06-23 17:21:36 -0300

    finaliza parte do bonus

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -0,0 +1,34 @@
+// 1 ° Questão:
+
+let n = 5
+
+// let square = ''
+
+// for(let linha = 0; linha <= n; linha += 1) {
+//     for(let coluna = 0; coluna <= n; coluna += 1) {
+//         square += '*'
+//     }
+//     console.log(square)
+//     square = ''
+// }
+
+// 2° Questão:
+
+// let rightTriangle = ''
+
+// for(index = 0; index <= n; index += 1) {
+//     rightTriangle += '*'
+//     console.log(rightTriangle)
+// }
+
+// 3° Questão:
+
+let rightTriangle = ''
+let espaco = ' '
+
+for(index = 0; index <= n; index += 1) {
+    for(indexEspaco = 0; indexEspaco <= n; indexEspaco +=1)
+    inde
+    rightTriangle += '*'
+    console.log(rightTriangle)
+}
